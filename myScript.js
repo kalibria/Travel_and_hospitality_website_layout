7f111986e41bcadc91dc8d9f8fb2b89eabcc782f
Maria Kazakova <mariakazakova92@gmail.com>
2024-04-19 20:02:14 +0300

    travel and hospitality website layout

---
+++ myScript.js
@@ -0,0 +1,50 @@
+const button1 = document.querySelector('#button1');
+const button2 = document.querySelector('#button2');
+const button3 = document.querySelector('#button3');
+const buttons = [button1, button2, button3]
+
+const content1 = document.querySelector('#content1');
+const content2 = document.querySelector('#content2');
+const content3 = document.querySelector('#content3');
+const contents = [content1, content2, content3];
+
+const object = {
+  'button1' : content1,
+  'button2': content2,
+  'button3': content3
+}
+
+
+const updateContent = (event) => {
+  const targetButton = event.target;
+  const clickedButtonId = targetButton.id;
+  console.log("class", clickedButtonId)
+
+  // Remove the 'active' class from all buttons
+  buttons.forEach(button => button.classList.remove('smallButton_active'));
+  contents.forEach(contentObj => contentObj.classList.remove('invisible'));
+
+  // Add the 'active' class to the clicked button
+  // targetButton.classList.add('active');
+  // object[clickedButtonId].classList.add('');
+
+  const contentsToBeMadeInvisible = Object.entries(object).reduce((acc, [buttonId, contentObj]) => {
+    if (buttonId === clickedButtonId) {
+      return acc;
+    }
+
+    acc.push(contentObj)
+
+    return acc;
+  }, [])
+
+  contentsToBeMadeInvisible.forEach((contentObj ) => {
+    contentObj.classList.add('invisible')
+  })
+}
+
+button1.addEventListener('click', updateContent)
+button2.addEventListener('click', updateContent)
+button3.addEventListener('click', updateContent)
+
+
